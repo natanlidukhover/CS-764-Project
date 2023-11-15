a49f2f6c8f4b27b142ef15e35b21d961a93b655f
tywilson12 <146108769+tywilson12@users.noreply.github.com>
2023-11-15 13:10:59 -0600

    Update Ssd.cpp

--- Ssd.cpp
+++ Ssd.cpp
@@ -1,36 +1,62 @@
 #include "Ssd.h"
-#include <cstdlib>
+#include <cstdio>
 #include <cstring>
 #include <iostream>
 
-Ssd::Ssd(size_t size) : _size(size), _sizeOccupied(0) {
-    startPtr = malloc(size);
-    if (startPtr == nullptr) {
-        throw std::bad_alloc();
+constexpr size_t PAGE_SIZE = 8192; //8KB of data
+
+class Ssd {
+public:
+    Ssd(const char* filename, size_t size);
+    ~Ssd();
+    void* writeData(const void* data);
+    void* readData(void* buffer, size_t offset);
+
+    size_t getReadCount() const { return _readCount; }
+    size_t getWriteCount() const { return _writeCount; }
+
+private:
+    FILE* filePtr;
+    size_t _size;
+    size_t _sizeOccupied;
+    size_t _readCount = 0;  // read call counter
+    size_t _writeCount = 0; // write call counter
+};
+
+Ssd::Ssd(const char* filename, size_t size) : _size(size), _sizeOccupied(0), _readCount(0), _writeCount(0) {
+    filePtr = fopen(filename, "INSERT FILE NAME"); // insert filename here
+    if (filePtr == nullptr) {
+        throw std::runtime_error("Failed to open file");
     }
 }
 
 Ssd::~Ssd() {
-    free(startPtr);
+    fclose(filePtr);
 }
 
-void* Ssd::writeData(const void* data, size_t size) {
-    if (_sizeOccupied + size > _size) {
+void* Ssd::writeData(const void* data) {
+    if (_sizeOccupied + PAGE_SIZE > _size) {
         return nullptr;
     }
-    void* writePtr = static_cast<uint8_t*>(startPtr) + _sizeOccupied;
-    memcpy(writePtr, data, size);
-    _sizeOccupied += size;
-
-    return writePtr;
+    fseek(filePtr, _sizeOccupied, SEEK_SET);
+    size_t written = fwrite(data, 1, PAGE_SIZE, filePtr);
+    if (written != PAGE_SIZE) {
+        return nullptr;
+    }
+    _sizeOccupied += PAGE_SIZE;
+    _writeCount++;  
+    return (void*)(_sizeOccupied - PAGE_SIZE);
 }
 
-void* Ssd::readData(void* buffer, size_t size, size_t offset) {
-    if (offset + size > _sizeOccupied) {
+void* Ssd::readData(void* buffer, size_t offset) {
+    if (offset + PAGE_SIZE > _sizeOccupied) {
         return nullptr;
     }
-    void* readPtr = static_cast<uint8_t*>(startPtr) + offset;
-    memcpy(buffer, readPtr, size);
+    fseek(filePtr, offset, SEEK_SET);
+    size_t read = fread(buffer, 1, PAGE_SIZE, filePtr);
+    if (read != PAGE_SIZE) {
+        return nullptr;
+    }
+    _readCount++;
     return buffer;
 }
-
